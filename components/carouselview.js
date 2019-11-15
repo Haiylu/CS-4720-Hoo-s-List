4c81bb61e5909e9677192c49448d7c5366af3944
Jack Renner <jrenner234@gmail.com>
2019-11-15 03:56:48 -0500

    Added profile changes, and created listingcard/carouselview
    I also added some FB login stuff but its not working yet so ignore it for now.

---
+++ components/carouselview.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import ListingCard from './listingcard';
+import { Dimensions } from 'react-native';
+
+export default function CaroselView(props) {
+  let deviceWidth = Dimensions.get('window').width;
+  let dummyData = [1,2,3,4]
+
+  function renderItem({ item, index }) {
+    return (
+      <ListingCard />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Carousel
+        data={dummyData}
+        renderItem={renderItem}
+        sliderHeight={200}
+        sliderWidth={deviceWidth}
+        itemWidth={deviceWidth * 0.8}
+        enableSnap={true}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: 300,
+  },
+});
---
+++ components/listingcard.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import desk from '../assets/desk.jpeg'
+
+const listing = {
+    id:'uniqueID',
+    img:'image source',
+    owner:{
+        name:'Jack Renner',
+    },
+    price:'$150.00',
+    type:'Furniture',
+    description:'This is a 2010 IKEA Malmo desk that is in great condition. It has 3 drawers, and plenty of space to work on.',
+    location:'199 Washington Ave.'
+}
+
+export default class ListingCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Image style={styles.itemImg} source={desk} />
+                <View style={styles.infoContainer}>
+                    <View style={styles.textContainer}>
+                        <Text style={styles.textStyle}>Seller: {listing.owner.name}</Text>
+                        <Text style={styles.textStyle}>Category: {listing.type}</Text>
+                        <Text style={styles.textStyle}>Price: {listing.price}</Text>
+                        <Text style={styles.textStyle}>Location: {listing.location}</Text>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity>
+                            <Ionicons name={'md-star'} size={35} color={'white'} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Ionicons name={'md-list'} size={35} color={'white'} />
+                        </TouchableOpacity> 
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        backgroundColor: '#247BA0',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#247BA0',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+        elevation: 1,
+        width:300,
+        height:230,
+        margin:10
+    },
+    infoContainer: {
+        flexDirection:'row',
+        justifyContent:'flex-start',
+        alignItems:'center',
+        width:'100%',
+        margin:5,
+    },
+    buttonContainer: {
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        alignItems:'center',
+        width:'40%'
+    },
+    textStyle: {
+        color:'white',
+        fontSize: 12,
+        fontFamily: 'Futura'
+    },
+    textContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        width:'60%'
+    },
+    itemImg: {
+        height: 150,
+        width: '100%',
+        borderRadius: 1,
+        borderColor: 'black',
+    },
+});
